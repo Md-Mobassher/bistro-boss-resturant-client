5dc8e5234312ee2ce205c564150b0f78d1fa7bfd
Md-Mobassher <you@example.com>
2023-10-16 18:20:03 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
+
+
+const Home = () => {
+    return (
+        <>
+            <Banner />
+            <Category />
+        </>
+    );
+};
+
+export default Home;
